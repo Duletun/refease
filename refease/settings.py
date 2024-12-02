2e43e1e204e44a8f3e7f4f3d28fc8219f6238a9b
Duletun <duletun@gmail.com>
2024-12-03 00:12:58 +0300

    added onrender support

--- refease/settings.py
+++ refease/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nxe2pba8_j=%urg0iml6ezkyyf0x8hmopg5ujfsv$o95y@dvf+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['refease.onrender.com']
 
 AUTH_USER_MODEL = 'referral_app.User'
 CACHES = {
